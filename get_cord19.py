dc2678b947d8f0c984911e4e5155fc9be9e87599
Tran <AH_Tran@hotmail.de>
2022-05-19 09:46:12 +0200

    Update

--- get_cord19.py
+++ get_cord19.py
@@ -2,7 +2,7 @@ import pyterrier as pt
 print("pt.init")
 pt.init()
 print("get dataset")
-dataset = pt.get_dataset('irds:cord19/trec-covid')
+dataset = pt.get_dataset('irds:cord19')
 # Index cord19
 print("create indexer")
 indexer = pt.IterDictIndexer('./indices2/cord19')
